b418d2a6cca8eff38e3f0f50e3d72f46a7c994bc
DAMontreal <dg@diversiteartistique.org>
2025-04-17 16:42:10 -0400

    Add files via upload

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Hero from "@/components/home/hero";
 import SearchSection from "@/components/home/search-section";
 import FeaturedArtists from "@/components/home/featured-artists";
@@ -7,6 +8,21 @@ import Testimonials from "@/components/home/testimonials";
 import JoinCta from "@/components/home/join-cta";
 
 const Home = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  // Utilisation d'un state pour s'assurer que le code ne s'exécute que côté client
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-16 h-16 border-t-4 border-dam-orange border-solid rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
     <>
       <Hero />
