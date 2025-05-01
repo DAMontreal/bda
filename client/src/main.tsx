f710c858565a6e2a998739735b9bf12847b9f9b3
DAMontreal <dg@diversiteartistique.org>
2025-05-01 11:17:47 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+// client/src/main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -5,7 +6,3 @@ import "./index.css";
 document.title = "Bottin des artistes | Diversité Artistique Montréal";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-import router from './router' // This should now correctly resolve to client/src/router/index.ts
-// ... other imports
-app.use(router)
