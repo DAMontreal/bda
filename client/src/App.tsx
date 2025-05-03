850698b6c88a846e36ccbcb62be90a10798aa313
DAMontreal <dg@diversiteartistique.org>
2025-05-02 22:22:37 -0400

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
+import MessageNotification from "@/components/messages/message-notification";
 
 // Layouts
 import Header from "@/components/layout/header";
@@ -13,6 +14,7 @@ import Home from "@/pages/home";
 import Artists from "@/pages/artists";
 import ArtistProfile from "@/pages/artist-profile";
 import Events from "@/pages/events";
+import EventDetail from "@/pages/event-detail";
 import TrocDam from "@/pages/trocdam";
 import About from "@/pages/about";
 import Register from "@/pages/register";
@@ -29,21 +31,22 @@ function Router() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow pt-16">
-<Switch>
-  <Route path="/" component={Home} />
-  <Route path="/artists" component={Artists} />
-  <Route path="/artists/:id" component={ArtistProfile} />
-  <Route path="/events" component={Events} />
-  <Route path="/trocdam" component={TrocDam} />
-  <Route path="/about" component={About} />
-  <Route path="/register" component={Register} />
-  <Route path="/login" component={Login} />
-  <Route path="/dashboard" component={Dashboard} />
-  <Route path="/messages/:id" component={Messages} /> {/* ✅ nouvelle route dynamique */}
-  <Route path="/messages" component={Messages} />
-  <Route path="/admin" component={Admin} />
-  <Route component={NotFound} />
-</Switch>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/artists" component={Artists} />
+          <Route path="/artists/:id" component={ArtistProfile} />
+          <Route path="/events" component={Events} />
+          <Route path="/events/:id" component={EventDetail} />
+          <Route path="/trocdam" component={TrocDam} />
+          <Route path="/about" component={About} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/messages" component={Messages} />
+          <Route path="/messages/:userId" component={Messages} />
+          <Route path="/admin" component={Admin} />
+          <Route component={NotFound} />
+        </Switch>
       </main>
       <Footer />
     </div>
@@ -55,6 +58,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Router />
+        <MessageNotification />
         <Toaster />
       </AuthProvider>
     </QueryClientProvider>
