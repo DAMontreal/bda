85d6580bfb5437c3a94d0a9eb6ceb73cb8db7991
DAMontreal <dg@diversiteartistique.org>
2025-07-18 11:39:11 -0400

    Add files via upload

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,29 +1,11 @@
-console.log("Page d’accueil chargée");
-import { useState, useEffect } from "react";
 import Hero from "@/components/home/hero";
 import SearchSection from "@/components/home/search-section";
 import FeaturedArtists from "@/components/home/featured-artists";
 import UpcomingEvents from "@/components/home/upcoming-events";
 import TrocDamPreview from "@/components/home/trocdam-preview";
-import Testimonials from "@/components/home/testimonials";
 import JoinCta from "@/components/home/join-cta";
 
 const Home = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  // Utilisation d'un state pour s'assurer que le code ne s'exécute que côté client
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="w-16 h-16 border-t-4 border-dam-orange border-solid rounded-full animate-spin"></div>
-      </div>
-    );
-  }
-
   return (
     <>
       <Hero />
@@ -31,7 +13,6 @@ const Home = () => {
       <FeaturedArtists />
       <UpcomingEvents />
       <TrocDamPreview />
-      <Testimonials />
       <JoinCta />
     </>
   );
